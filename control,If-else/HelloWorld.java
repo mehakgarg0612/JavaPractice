c420c5241b7f45dfc2a6ae0032ce561f5e7eb806
Mehak garg <mehakgarg9876@gmail.com>
2025-02-18 06:52:12 +0530

    oddEven

--- control,If-else/HelloWorld.java
+++ control,If-else/HelloWorld.java
@@ -1,11 +1,31 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+// public class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+// }
 
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello2222, World!");
+// public class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println("Hello2222, World!");
+//     }
+// }
+
+// for(int i = 1;i<=5;i++)
+// {
+//     System.out.println("I will not eat all the cookies 🍪 " + i);
+// }
+
+// Write a program to check whether a number is even or odd.
+
+class HelloWorld {
+    public static void main(String[] args){
+        int num = 2;
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+
     }
 }
