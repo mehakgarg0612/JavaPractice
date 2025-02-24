9e872c52865a6f86eb9665a4e1cdb639dc1af35f
Mehak Garg <mehak.garg@idsil.loc>
2025-02-24 13:03:24 +0530

    control_oddeven

--- control,If-else/HelloWorld.java
+++ control,If-else/HelloWorld.java
@@ -1,27 +1,9 @@
-/*public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello2222, World!");
-    }
-}
-
-for(int i = 1;i<=5;i++)
-{
-    System.out.println("I will not eat all the cookies 🍪 " + i);
-}
-
-Write a program to check whether a number is even or odd.
-*/
-class HelloWorld {
     static  Boolean  num;
     public static void main(String[] args){
-        EvenOdd();
-        //DataType();
+        //EvenOdd();
+        DataType();
         
         System.out.println(num);
 
@@ -37,21 +19,5 @@ class HelloWorld {
         System.out.println("hello my name is sunil");
         System.out.print("hello my name is mehak");
         System.out.println(myNum+" "+myBool);
-    
-
         }
-    
-    public static void EvenOdd()
-    {
-            String nae=null;
-        int num = 2;
-        String name="Mehak";
-        if (num % 2 == 0) {
-            System.out.println("Even");
-        }
-        else{
-            System.out.println("Odd");
-        }
-
     }
-}
